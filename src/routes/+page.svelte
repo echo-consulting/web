da73434521529c63aa1a4209d46a36f2bb5023b2
Ole Magnus Fon Johnsen <me@omfj.no>
2024-02-28 12:23:25 +0100

    🎉 Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const EMAIL = 'consulting@echo.uib.no';
+</script>
+
+<!-- You may not like, but this is peak web design -->
+
+<h1>echo Consulting</h1>
+
+<p>
+	Har du et prosjekt som trenger dyktige utviklere? echo Consulting tilbyr konsulenttjenester innne
+	IT og webutvikling, uten at det koster skjorta.
+</p>
+
+<p>
+	Kontakt oss på <a href="mailto:{EMAIL}">{EMAIL}</a> for en uforpliktende prat.
+</p>
