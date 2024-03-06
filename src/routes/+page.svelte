62519b795b8a092f4e0ce0157e91d75c2bf32187
Ole Magnus Fon Johnsen <me@omfj.no>
2024-03-06 02:37:24 +0100

    💄 Update style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,23 @@
 
 <!-- You may not like, but this is peak web design -->
 
-<h1>echo Consulting</h1>
+<div>
+	<h1>echo Consulting</h1>
 
-<p>
-	Har du et prosjekt som trenger dyktige utviklere? echo Consulting tilbyr konsulenttjenester innen
-	IT og web-utvikling, uten at det koster deg skjorta.
-</p>
+	<p>
+		Har du et prosjekt som trenger dyktige utviklere? echo Consulting tilbyr konsulenttjenester
+		innen IT og web-utvikling, uten at det koster deg skjorta.
+	</p>
 
-<p>
-	Kontakt oss på <a href="mailto:{EMAIL}">{EMAIL}</a> for en uforpliktende prat.
-</p>
+	<p>
+		Kontakt oss på <a href="mailto:{EMAIL}">{EMAIL}</a> for en uforpliktende prat.
+	</p>
+</div>
+
+<style>
+	div {
+		max-width: 768px;
+		margin: 0 auto;
+		padding: 1rem;
+	}
+</style>
