8ae4682464541e3168f6c0451f8e724dc6bcb138
Ole Magnus Fon Johnsen <me@omfj.no>
2024-03-28 08:38:44 +0100

    🌥️ Switch to Cloudflare

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,23 @@
 <script lang="ts">
+	import Logo from '$lib/assets/echo-consulting-logo.png';
+
 	const EMAIL = 'consulting@echo.uib.no';
 </script>
 
-<!-- You may not like, but this is peak web design -->
+<div class="py-24 space-y-14 px-6 min-h-screen">
+	<img src={Logo} alt="Echo Consulting Logo" class="w-auto mx-auto h-[min(35vw,160px)]" />
 
-<div>
-	<h1>echo Consulting</h1>
+	<div class="space-y-4 max-w-screen-sm mx-auto text-center">
+		<p class="text-balance">
+			Har du et prosjekt som trenger dyktige utviklere? echo Consulting tilbyr konsulenttjenester
+			innen IT og web-utvikling, uten at det koster deg skjorta.
+		</p>
 
-	<p>
-		Har du et prosjekt som trenger dyktige utviklere? echo Consulting tilbyr konsulenttjenester
-		innen IT og web-utvikling, uten at det koster deg skjorta.
-	</p>
-
-	<p>
-		For øyeblikket ser vi etter nye medlemmer. <a href="/sok">Søk her</a>!
-	</p>
-
-	<p>
-		Kontakt oss på <a href="mailto:{EMAIL}">{EMAIL}</a> for en uforpliktende prat.
-	</p>
+		<a
+			class="relative px-4 border border-[#212121] rounded-md bg-[#181818] font-medium tracking-wide h-10 w-fit mx-auto flex items-center justify-center text-[#dedede] hover:bg-[#212121] transition-colors duration-200 hover:text-[#eeeeee] ease-in-out"
+			href="mailto:{EMAIL}"
+		>
+			Ta kontakt
+		</a>
+	</div>
 </div>
-
-<style>
-	div {
-		max-width: 768px;
-		margin: 0 auto;
-		padding: 1rem;
-	}
-</style>
--- src/routes/sok/+server.ts
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = () => {
-	return Response.redirect('https://forms.gle/J7jy7EPrBrKX2Rej6');
-};
