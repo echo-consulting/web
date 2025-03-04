e564f50b23c47014c48ab90f42bd8621618c7eb3
SebastianJM <sebastianjacmatt@gmail.com>
2025-03-04 16:35:16 +0100

    hero component

---
+++ app/components/hero.tsx
@@ -0,0 +1,39 @@
+import { Container, Title, Button, Text, Box } from "@mantine/core";
+import { Typewriter } from "react-simple-typewriter";
+import { useState } from "react";
+
+export function Hero() {
+    const [fin, setFin] = useState(false);
+    return (
+        <Container className="text-white text-left">
+            <Title order={1} className="text-5xl h-28 max-w-[25rem] font-bold pb-8 whitespace-pre-line">
+                {fin ? (
+                        "Konsulenter som ikke koster skjorta."
+                    ) : (
+                        <Typewriter 
+                            words={[
+                                "echo\nConsulting",
+                                "Konsulenter-tjenester som ikke koster skjorta.",
+                                "echo\nConsulting"
+                            ]}
+                            loop={1}
+                            cursor
+                            cursorStyle="|"
+                            onLoopDone={() => setFin(true)}
+                        />
+                    )}
+            </Title>
+            <Box className="max-w-lg pb-4 block">
+                <Text size="lg">
+                    Vi tilbyr profesjonelle konsulenttjenester til rimelige priser, skreddersydd for dine behov. 
+                    La oss hjelpe deg med å nå dine mål uten å sprenge budsjettet!
+                </Text>
+            </Box>
+
+            <Button>
+                Kontakt oss
+            </Button>
+
+        </Container>
+    );
+}
