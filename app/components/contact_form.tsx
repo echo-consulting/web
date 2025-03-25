eafbf86651eb7d83f8685ebb0e44f9ffca1d5b30
Erik <erik.g.ingebrigtsen@gmail.com>
2025-03-25 18:48:34 +0100

    lagt til inputfelt med navn og send ikon på knapp

--- app/components/contact_form.tsx
+++ app/components/contact_form.tsx
@@ -1,24 +1,28 @@
-import { Container, Title, Button, Text, Box, TextInput, Textarea, Group } from "@mantine/core";
-import { Notifications, showNotification } from "@mantine/notifications";
+import { Container, Title, Button, TextInput, Textarea, Group, GridCol, Grid } from "@mantine/core";
+import { showNotification } from "@mantine/notifications";
 import { useForm } from "@mantine/form";
+import { IoSend } from "react-icons/io5";
 
 export function ContactForm() {
     const form = useForm({
         mode: "uncontrolled",
         initialValues: {
+            name: "",
             email: "",
             message: "",
         },
         validate: {
+            name: (value) => (value.trim().length > 0 ? null : 'Name is required'),
             email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
         }
     });
 
     const handleSubmit = (values: { email: string; message: string }) => {
+        {/* handle form data here */}
         console.log(values);
         showNotification({
             title: "✅ Meldingen din er sendt!",
-            message: "Sjekk inboxen din, du skal motta en bekreftelse på e-post snart.",
+            message: "Vi vil ta kontakt med deg så snart som mulig.",
             color: "green",
         });
     }
@@ -30,11 +34,18 @@ export function ContactForm() {
                 Kontakt oss
             </Title>
         <form onSubmit={form.onSubmit(handleSubmit)}>
-            <TextInput withAsterisk label="E-post" key={form.key("email")}{...form.getInputProps("email")} placeholder="ola.nordmann@firma.no"/>
+            <Grid>
+                <Grid.Col span={6}>
+                    <TextInput withAsterisk label="Navn" key={form.key("name")}{...form.getInputProps("name")} placeholder="Ola Nordmann"/>
+                </Grid.Col>
+                <Grid.Col span={6}>
+                    <TextInput withAsterisk label="E-post" key={form.key("email")}{...form.getInputProps("email")} placeholder="ola.nordmann@firma.no"/>
+                </Grid.Col>
+            </Grid>
             <Textarea label="Melding" key={form.key("message")}{...form.getInputProps("message")} __size="" autosize minRows={4} placeholder="Hei, Echo Consulting! Jeg er interessert i deres konsulenttjenester og ønsker mer informasjon. Vennligst ta kontakt med meg!"/>    
         
             <Group justify="flex-end" mt="md">
-                <Button color="blue" className="mt-8" type="submit">
+                <Button leftSection={<IoSend/>} color="blue" className="mt-8" type="submit">
                 Send
                 </Button>
             </Group>
