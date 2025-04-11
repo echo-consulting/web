f4bceb71e23900a57cf9e8c92979244e02b711da
Benjamin <benjamin.guls@gmail.com>
2025-04-11 12:21:04 +0200

    style spacing using css

--- app/routes/om-oss.tsx
+++ app/routes/om-oss.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Title, Text, Anchor, Container } from "@mantine/core";
+import { Title, Text, Anchor, BackgroundImage, Container } from "@mantine/core";
 import { Link } from "react-router";
 import members from "../assets/medlemmer.png";
 
@@ -15,19 +15,19 @@ export function meta({}: Route.MetaArgs) {
 
 export default function AboutUs() {
   return (
-    <Container className="relative z-10 pt-30 text-white text-left">
+    <Container className="relative z-10 pt-30 text-white text-left space-y-10">
       <Title
         order={1}
         className="text-5xl max-w-[25rem] font-bold pb-8 whitespace-pre-line"
       >
         Om echo Consulting
       </Title>
-      <Text>
+      <Text style={{ marginBottom: "1rem" }}>
         echo Consulting er en undergruppe av linjeforeningen for informatikk ved
         Universitetet i Bergen. Gruppen består av 10 engasjerte
         informatikkstudenter med stor interesse for web-utvikling.
-        <br />
-        <br />
+      </Text>
+      <Text>
         Vi tilbyr rådgivning og løsninger innen teknologi og digital utvikling.
         Gjennom echo Consulting bygger vi bro mellom teori og praksis, samtidig
         som vi hjelper bedrifter med å nå sine mål.
@@ -36,11 +36,11 @@ export default function AboutUs() {
         {" "}
         Sjekk ut hva vi kan tilby din organisasjon her!
       </Anchor>
-      <br />
-      <br />
+
       <Title
         order={2}
-        className="text-5xl max-w-[25rem] font-bold pb-2 whitespace-pre-line"
+        className="text-5xl max-w-[25rem] font-bold pb-2"
+        style={{ marginTop: "1rem" }}
       >
         Medlemmer 2024/2025
       </Title>
