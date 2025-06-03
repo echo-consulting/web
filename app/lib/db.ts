bc5a00d9f3c14a6277010001c72c2115dc490002
Erik <erik.g.ingebrigtsen@gmail.com>
2025-06-03 17:40:59 +0200

    add database

---
+++ app/lib/db.ts
@@ -0,0 +1,12 @@
+import {config } from 'dotenv';
+import { drizzle } from 'drizzle-orm/libsql';
+
+config({ path: '.dev.vars'});
+
+// You can specify any property from the libsql connection options
+const db = drizzle({ 
+  connection: { 
+    url: process.env.TURSO_DATABASE_URL!, 
+    authToken: process.env.TURSO_AUTH_TOKEN!
+  }
+});
