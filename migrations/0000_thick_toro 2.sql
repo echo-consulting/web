d4fb86d62c4c65a57d1c3877d205a6d56e0028f7
Erik <erik.g.ingebrigtsen@gmail.com>
2025-06-03 21:44:52 +0200

    Revert "liten e"
    This reverts commit fed5c7d677cab18a89ba5eaf2ab0566602af7f41.

--- migrations/0000_thick_toro 2.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `contact_table` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`name` text NOT NULL,
-	`email` text NOT NULL,
-	`content` text NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `contact_table_email_unique` ON `contact_table` (`email`);
