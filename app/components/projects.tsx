3395b510071bad7cb655d3a9a0cdf5c2378c4402
Max Bakke Seymour <max.bakke.seymour@gmail.com>
2025-09-19 19:06:39 +0200

    Add image data for LFFH

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,22 +1,22 @@
 import { Container, Title } from "@mantine/core";
 import { ArrowRight } from "lucide-react";
 import { useState } from "react";
+import { Link } from "react-router";
 
 // Dette skal hentes fra Sanity etterhvert
 const projectData = [
   {
     title: "LFFH-nettsiden",
     desc: "Funksjonell nettside for Linjeforeningen for Fiskehelse og Havbruk, men mulighet for blant annet innlogging, arrangementopprettelse og påmelding til arrangementer",
-    image:
-      "https://www.alvdalmiv.no/wp-content/uploads/2017/04/Bilder_2017_Uke13_fgp-1024x576.jpg", // TODO
+    image: "../../public/lffh_logo.png",
   },
+  /*
   {
     title: "Prosjekt2",
     desc: "bla bla",
     image:
       "https://www.visitjotunheimen.no/dmsimgs/Flaklypa_Grand_Prix_16x9_1966284657.jpg",
   },
-  /*
   {
     title: "Prosjekt3",
     desc: "bla bla",
@@ -51,7 +51,10 @@ export default function Projects() {
               {project.title}
             </h3>
             <p className="text-[#CFCDCD] tracking-wider">{project.desc}</p>
-            <h4 className="flex items-center gap-2 font-medium tracking-wider mt-2 group">
+            <Link
+              to={`projects/${index}`}
+              className="flex items-center gap-2 font-medium tracking-wider mt-2 group"
+            >
               Les mer her
               <span
                 className="inline-flex transition-transform duration-300"
@@ -62,7 +65,7 @@ export default function Projects() {
               >
                 <ArrowRight size={24} />
               </span>
-            </h4>
+            </Link>
           </div>
         ))}
       </div>
